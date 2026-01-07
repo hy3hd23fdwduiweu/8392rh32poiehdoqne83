9fdbb337bac7a39ca636d810ecf1f8f8c235ee11
hy3hd23fdwduiweu <baldeagle5013mc@gmail.com>
2026-01-08 00:06:49 +0100

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -5,13 +5,15 @@ import { v4 as uuidv4 } from "uuid";
 const r = express.Router();
 
 r.post("/", (req, res) => {
-  const { uuid, name } = req.body;
+  const { uuid, name, server } = req.body;
+  if (!uuid || !server) return res.sendStatus(400);
+
   const token = uuidv4();
 
   db.run(
-    `INSERT OR IGNORE INTO players (uuid,name,token)
-     VALUES (?,?,?)`,
-    [uuid, name, token]
+    `INSERT OR IGNORE INTO players (uuid,server,name,token)
+     VALUES (?,?,?,?)`,
+    [uuid, server, name, token]
   );
 
   res.json({ token });
