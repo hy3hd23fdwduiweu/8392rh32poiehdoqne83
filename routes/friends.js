116f1e079606775228f11fb31ec599415e5824d8
hy3hd23fdwduiweu <baldeagle5013mc@gmail.com>
2026-01-08 00:07:14 +0100

    Update friends.js

--- routes/friends.js
+++ routes/friends.js
@@ -4,18 +4,27 @@ import { db } from "../db.js";
 const r = express.Router();
 
 r.post("/request", (req, res) => {
-  db.run(
-    `INSERT OR IGNORE INTO friends VALUES (?,?,0)`,
-    [req.player.uuid, req.body.target]
+  db.get(
+    `SELECT COUNT(*) as c FROM friends
+     WHERE server=? AND (sender=? OR receiver=?) AND accepted=1`,
+    [req.server, req.player.uuid, req.player.uuid],
+    (_, row) => {
+      if (row.c >= 6) return res.status(403).json({ error: "Friend limit" });
+
+      db.run(
+        `INSERT OR IGNORE INTO friends VALUES (?,?,?,0)`,
+        [req.server, req.player.uuid, req.body.target]
+      );
+      res.sendStatus(200);
+    }
   );
-  res.sendStatus(200);
 });
 
 r.post("/accept", (req, res) => {
   db.run(
     `UPDATE friends SET accepted=1
-     WHERE sender=? AND receiver=?`,
-    [req.body.sender, req.player.uuid]
+     WHERE server=? AND sender=? AND receiver=?`,
+    [req.server, req.body.sender, req.player.uuid]
   );
   res.sendStatus(200);
 });
@@ -23,9 +32,9 @@ r.post("/accept", (req, res) => {
 r.post("/remove", (req, res) => {
   db.run(
     `DELETE FROM friends
-     WHERE (sender=? AND receiver=?)
-     OR (sender=? AND receiver=?)`,
-    [req.player.uuid, req.body.target, req.body.target, req.player.uuid]
+     WHERE server=? AND
+     ((sender=? AND receiver=?) OR (sender=? AND receiver=?))`,
+    [req.server, req.player.uuid, req.body.target, req.body.target, req.player.uuid]
   );
   res.sendStatus(200);
 });
