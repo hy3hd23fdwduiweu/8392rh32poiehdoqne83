63b58d745bcfac83ae9ade7e0009948bb46d61bc
hy3hd23fdwduiweu <baldeagle5013mc@gmail.com>
2026-01-07 23:54:39 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import "./db.js";
+import { auth } from "./auth.js";
+
+import register from "./routes/register.js";
+import location from "./routes/location.js";
+import friends from "./routes/friends.js";
+import shares from "./routes/shares.js";
+import groups from "./routes/groups.js";
+import stealth from "./routes/stealth.js";
+import visibility from "./routes/visibility.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/register", register);
+app.use("/location", auth, location);
+app.use("/friends", auth, friends);
+app.use("/shares", auth, shares);
+app.use("/groups", auth, groups);
+app.use("/stealth", auth, stealth);
+app.use("/visible", auth, visibility);
+
+app.listen(3000, () => console.log("TeamTracker API running"));
