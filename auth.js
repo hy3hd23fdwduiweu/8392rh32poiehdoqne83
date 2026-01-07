523a1ee4a99587773712b20b64ec94088e0be3b2
hy3hd23fdwduiweu <baldeagle5013mc@gmail.com>
2026-01-08 00:06:30 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,14 +2,17 @@ import { db } from "./db.js";
 
 export function auth(req, res, next) {
   const token = req.headers["x-token"];
-  if (!token) return res.sendStatus(401);
+  const server = req.headers["x-server"];
+
+  if (!token || !server) return res.sendStatus(401);
 
   db.get(
-    "SELECT * FROM players WHERE token=?",
-    [token],
+    "SELECT * FROM players WHERE token=? AND server=?",
+    [token, server],
     (_, row) => {
       if (!row) return res.sendStatus(403);
       req.player = row;
+      req.server = server;
       next();
     }
   );
